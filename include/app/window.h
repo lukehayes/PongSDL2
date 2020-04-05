77afb12aa0d5b3948abcd70b0245fe9280a2d7ee
lukehayes <lukepurplehayes@gmail.com>
2020-04-05 14:11:30 +0100

    Added include and src dirs

---
+++ include/app/window.h
@@ -0,0 +1,12 @@
+#ifndef APP_WINDOW_H
+#define APP_WINDOW_H
+
+namespace App {
+
+	class Window {
+		Window() {}
+		~Window() {}
+	};
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "SDL.h"
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL Not Initialized!" << std::endl;
+		return 1;
+	}
+
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_RESIZABLE, &window, &renderer);
+
+	SDL_Event event;
+
+	while (1) {
+
+		SDL_PollEvent(&event);
+		if (event.type == SDL_QUIT) {
+			break;
+		}
+
+		SDL_SetRenderDrawColor(renderer, 0, 150, 200, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
+
