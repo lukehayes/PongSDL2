4595d8a48e64cff5a508334f4f28c583960f7fc4
lukehayes <lukepurplehayes@gmail.com>
2020-05-27 21:35:49 +0100

    Entity class now inherits from SDL_Rect instead of having it as a member

--- include/entity/entity.h
+++ include/entity/entity.h
@@ -5,23 +5,14 @@
 
 namespace Entity {
 
-	class Entity {
+	class Entity : public SDL_Rect {
 
 	public:
 		Entity(int x, int y);
 		Entity(const Entity& rhs);
 		Entity& operator=(const Entity& rhs);
 		~Entity();
-
-		SDL_Rect getRect() const;
-
-		int x;
-		int y;
-		int w;
-		int h;
-
 	private:
-		SDL_Rect rect;
 
 	};
 }
--- src/entity/entity.cpp
+++ src/entity/entity.cpp
@@ -3,14 +3,20 @@
 
 namespace Entity {
 
-	Entity::Entity(int x, int y) : x(x), y(y), w(10), h(10)
+	Entity::Entity(int x, int y)
 	{
-		this->rect = { x,y,w,h };
+		this->x = x;
+		this->y = y;
+		this->w = 10;
+		this->h = 10;
 	}
 
-	Entity::Entity(const Entity& rhs) : x(rhs.x), y(rhs.y), w(rhs.w), h(rhs.h)
+	Entity::Entity(const Entity& rhs)
 	{
-		this->rect = { x,y,w,h };
+		this->x = rhs.x;
+		this->y = rhs.y;
+		this->w = rhs.w;
+		this->h = rhs.h;
 	}
 
 	Entity& Entity::operator=(const Entity& rhs) 
@@ -23,11 +29,5 @@ namespace Entity {
 		return *this;
 	}
 
-
 	Entity::~Entity() {}
-
-	SDL_Rect Entity::getRect() const
-	{
-		return this->rect;
-	}
 }
