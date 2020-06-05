3bc41caf69f5075dcbb83ef321e6f210ded7faba
lukehayes <lukepurplehayes@gmail.com>
2020-06-05 22:24:03 +0100

    Added Math::Util.h

---
+++ include/math/util.h
@@ -0,0 +1,15 @@
+#ifndef MATH_UTIL_H
+#define MATH_UTIL_H
+
+#include "entity/entity.h";
+#include <iostream>
+
+namespace Math {
+	
+	bool moveTo(Entity::Entity& entity, int x, int y, double dt) {
+		entity.x -= x * dt;
+		entity.y -= y * dt;
+	}
+}
+
+#endif
